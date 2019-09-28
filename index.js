01807b3838898aa97cb0fadde8a578505b61f62d
ignacio jocano <ignacio.jocano@gmail.com>
2019-09-28 12:57:04 -0300

    fix typo on server indexjs

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if (process.env.NODE_EN === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets like main.js or main.css
     app.use(express.static('client/build'));
 
